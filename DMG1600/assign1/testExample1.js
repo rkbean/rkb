831c58ccdaea0ab0a7c671dc9732f72f040bdabd
rkbean <rkbean@gmail.com>
2018-01-21 22:58:08 -0700

    Initial version

---
+++ DMG1600/assign1/testExample1.js
@@ -0,0 +1,8 @@
+/*jslint devel: true, node: true */
+
+var firstName, lastName;
+
+firstName = prompt('What is your first name');
+lastName = prompt('last name ?');
+alert('His first name is ' + firstName);
+alert('full name is ' + firstName + ' ' + lastName);
